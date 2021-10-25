0e4390253784ca421d552107856f3269e1c5ff09
ashish <ashishsachan919@gmail.com>
2021-10-25 21:49:44 +0530

    1. Add Live and Backtest Configuration

--- examples/examples.cpp
+++ examples/examples.cpp
@@ -11,9 +11,10 @@ using namespace kdb;
 #include <unordered_map>
 #include <random>
 #include <thread>
+#include "benchmark/benchmark.h"
 
-static constexpr int N = 1e6;
-struct test{
+static constexpr int N = 10000;
+struct test {
     std::vector<bool> cp;
     std::vector<int> strike;
     std::vector<double> iv;
@@ -43,33 +44,30 @@ struct data {
 };
 KDB_REGISTER(data,oiv,om,oweight);
 
-void testInsert(){
-    auto t= khp("localhost",6055);
+void testInsert(benchmark::State &state) {
+    auto t = khp("localhost", 6055);
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<float> dist(1.0, 10.0);
-    auto tableName=ks((S) "ivol");
-    for(int i = 0;i<N;i++){
-        simpleDict exp = {i,"NIFTY",dist(mt),{true,false},{10*i,10*i+1},{dist(mt),dist(mt)}};
-        //simpleTest exp = {i,"NIFTY",dist(mt)};
-        auto x = convert::from_native(exp);
-        k(-t,".u.upd",r1(tableName),x,K(0));
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    }
+    auto tableName = ks((S) "ivol");
+    for (auto _: state)
+        for (int i = 0; i < N; i++) {
+            simpleDict exp = {i, "NIFTY", dist(mt), {true, false}, {10 * i, 10 * i + 1}, {dist(mt), dist(mt)}};
+            //simpleTest exp = {i,"NIFTY",dist(mt)};
+            auto x = convert::from_native(exp);
+            k(-t, ".u.upd", r1(tableName), x, K(0));
+            //std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        }
 }
 
-const data& testSelect(){
-    auto t= khp("localhost",6055);
-    K _data=k(t,"exec from 1#select iv,m,weight from curve where date=min date,minute > 09:20 ",(K)0);
+const data &testSelect() {
+    auto t = khp("localhost", 6055);
+    K _data = k(t, "exec from 1#select iv,m,weight from curve where date=min date,minute > 09:20 ", (K) 0);
     K rows = kK(_data)[1];
     data test;
-    convert::to_native(rows,test);
+    convert::to_native(rows, test);
     return std::move(test);
 }
 
-
-int main(){
-    testInsert();
-    //testSelect();
-    return 0;
-}
+BENCHMARK(testInsert);
+BENCHMARK_MAIN();
